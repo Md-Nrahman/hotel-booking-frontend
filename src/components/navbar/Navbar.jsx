2dcedc7526f22119ee6f95259da69b6a135f7bcc
Naimur Rahman <naimur.rahman@suffixit.com>
2023-12-17 10:45:01 +0600

    Brushup

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,29 @@
-import { useContext } from "react"
-import { useNavigate } from "react-router-dom"
-import { AuthContext } from "../../context/AuthContext"
-import "./navbar.css"
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import "./navbar.css";
 
 const Navbar = () => {
-
-  const {user} = useContext(AuthContext)
-  const navigate = useNavigate()
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   return (
     <div className="navbar">
       <div className="navContainer">
         <span className="logo">lamabooking</span>
-       {user? user?.username:  <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton" onClick={()=>navigate('/login')}>Login</button>
-        </div>}
+        {user ? (
+          user?.username
+        ) : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton" onClick={() => navigate("/login")}>
+              Login
+            </button>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
